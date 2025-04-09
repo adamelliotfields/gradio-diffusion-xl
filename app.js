5b83335606f4c2eb3a8a7186d214b75bb235edf8
Adam Fields <me@aef.me>
2025-04-09 12:22:39 -0400

    Remove custom JS

--- app.js
+++
@@ -1,8 +0,0 @@
-() =>{
-  if (window.location.hostname.endsWith(".hf.space")) {
-    const hfHeader = document.getElementById("huggingface-space-header");
-    if (hfHeader) {
-      hfHeader.style.display = "none";
-    }
-  }
-}
