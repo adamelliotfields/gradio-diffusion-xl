eb180dd20fff3dd937efa413660f249f4def02ed
Adam Fields <me@aef.me>
2025-04-09 12:24:38 -0400

    Inline custom HTML

--- app.py
+++ app.py
@@ -2,16 +2,13 @@ import argparse
 import os
 from importlib.util import find_spec
 
-# Improved GPU handling and progress bars
-os.environ["ZEROGPU_V2"] = "1"
-
 # Use Rust-based downloader
 if find_spec("hf_transfer"):
     os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
 
 import gradio as gr
-from huggingface_hub._snapshot_download import snapshot_download
 
+# from huggingface_hub._snapshot_download import snapshot_download
 from lib import (
     Config,
     generate,
@@ -68,10 +65,37 @@ random_prompt_js = f"""
 }}
 """
 
+# Gradio styles
+head_html = """
+<style>
+  @media (min-width: 1536px) {
+    gradio-app > .gradio-container {
+      max-width: 1280px !important;
+    }
+  }
+</style>
+"""
+
+# Header
+header_html = """
+<div id="header">
+  <div>
+    <h1>
+      Diffusion&nbsp;<span>XL</span>
+    </h1>
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 15 15">
+      <path d="M7.48877 6.75C7.29015 6.75 7.09967 6.82902 6.95923 6.96967C6.81879 7.11032 6.73989 7.30109 6.73989 7.5C6.73989 7.69891 6.81879 7.88968 6.95923 8.03033C7.09967 8.17098 7.29015 8.25 7.48877 8.25C7.68738 8.25 7.87786 8.17098 8.0183 8.03033C8.15874 7.88968 8.23764 7.69891 8.23764 7.5C8.23764 7.30109 8.15874 7.11032 8.0183 6.96967C7.87786 6.82902 7.68738 6.75 7.48877 6.75ZM7.8632 0C11.2331 0 11.3155 2.6775 9.54818 3.5625C8.80679 3.93 8.47728 4.7175 8.335 5.415C8.69446 5.565 9.00899 5.7975 9.24863 6.0975C12.0195 4.5975 15 5.19 15 7.875C15 11.25 12.3265 11.325 11.4428 9.5475C11.0684 8.805 10.2746 8.475 9.57813 8.3325C9.42836 8.6925 9.19621 9 8.89665 9.255C10.3869 12.0225 9.79531 15 7.11433 15C3.74438 15 3.67698 12.315 5.44433 11.43C6.17823 11.0625 6.50774 10.2825 6.65751 9.5925C6.29056 9.4425 5.96855 9.2025 5.72891 8.9025C2.96555 10.3875 0 9.8025 0 7.125C0 3.75 2.666 3.6675 3.54967 5.445C3.92411 6.1875 4.71043 6.51 5.40689 6.6525C5.54918 6.2925 5.78882 5.9775 6.09586 5.7375C4.60559 2.97 5.1972 0 7.8632 0Z"></path>
+    </svg>
+  </div>
+  <p>
+    Image generation studio for Stable Diffusion XL.
+  </p>
+</div>
+"""
+
 with gr.Blocks(
-    head=read_file("./partials/head.html"),
+    head=head_html,
     css="./app.css",
-    js="./app.js",
     theme=gr.themes.Default(
         # colors
         neutral_hue=gr.themes.colors.gray,
@@ -92,7 +116,7 @@ with gr.Blocks(
         block_background_fill_dark=gr.themes.colors.gray.c900,
     ),
 ) as demo:
-    gr.HTML(read_file("./partials/intro.html"))
+    gr.HTML(header_html)
 
     with gr.Tabs():
         with gr.TabItem("🏠 Home"):
@@ -306,20 +330,21 @@ with gr.Blocks(
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(add_help=False, allow_abbrev=False)
-    parser.add_argument("-s", "--server", type=str, metavar="STR", default="0.0.0.0")
+    parser.add_argument("-s", "--server", type=str, metavar="STR", default="127.0.0.1")
     parser.add_argument("-p", "--port", type=int, metavar="INT", default=7860)
     args = parser.parse_args()
 
-    token = os.environ.get("HF_TOKEN", None)
-    for repo_id, allow_patterns in Config.HF_REPOS.items():
-        snapshot_download(
-            repo_id=repo_id,
-            repo_type="model",
-            revision="main",
-            token=token,
-            allow_patterns=allow_patterns,
-            ignore_patterns=None,
-        )
+    # Download all models from Hugging Face
+    # token = os.environ.get("HF_TOKEN", None)
+    # for repo_id, allow_patterns in Config.HF_REPOS.items():
+    #     snapshot_download(
+    #         repo_id=repo_id,
+    #         repo_type="model",
+    #         revision="main",
+    #         token=token,
+    #         allow_patterns=allow_patterns,
+    #         ignore_patterns=None,
+    #     )
 
     # https://www.gradio.app/docs/gradio/interface#interface-queue
     demo.queue(default_concurrency_limit=1).launch(
